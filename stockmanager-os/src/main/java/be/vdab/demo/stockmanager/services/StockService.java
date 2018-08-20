1274c90b7ff13b2e095cc84138639580ebc67c7c
Christian Bonami <cbonami@gmail.com>
2018-08-20 16:15:18 +0200

    Migrated to RHOAR (Spring Boot 1.5.8, Jenkinsfile, etc)

--- stockmanager-os/src/main/java/be/vdab/demo/stockmanager/services/StockService.java
+++ stockmanager-os/src/main/java/be/vdab/demo/stockmanager/services/StockService.java
@@ -27,7 +27,7 @@ public class StockService {
     }
 
     public Stock getStock(String productId) throws StockNotFoundException {
-        return stockRepository.findById(productId)
+        return Optional.ofNullable(stockRepository.findOne(productId))
                 .orElseThrow(() -> new StockNotFoundException("Stock not found with productId: " + productId));
     }
 }
